4776fba6b11487c633de6bd2f3cb6fb510dd43bb
skipper <skipper>
2021-11-16 15:01:26 +0800

    feat: combination a

--- src/combination/a.ts
+++ src/combination/a.ts
@@ -1,22 +1,36 @@
-// f(n) = n * (n - 1) * (n - 2) * ... * 1
-export function combinationA(numbers: number[]): number[][] {
-  const len = numbers.length;
+// 排列组合、顺序敏感
+// a(n) = n * (n - 1) * (n - 2) * ... * 1 
+// f(n) = a(n) + n * f(n - 1)
+function combinationA(numbers: number[]): number[][] {
   const combinations: number[][] = [];
-  for (let i = 0; i < len; i++) {
-    const current = numbers.slice(0, i + 1);
-    combinations.push([...current]);
-    for (let j = 0; j < current.length; j++) {
-      for (let k = j + 1; k < numbers.length; k++) {
-        current[j] = numbers[k];
+  if (numbers.length === 0) return [];
+  if (numbers.length === 1) return [numbers];
+  for (let i = 0; i < numbers.length; i++) {
+    const copy = [...numbers];
+    copy.splice(i, 1);
+    const combinationsNMinuseOne = combinationA(copy);
+    combinationsNMinuseOne.forEach((ele) => {
+      combinations.push(ele);
+      if (ele.length === numbers.length - 1) {
+        combinations.push([numbers[i], ...ele]);
       }
-    }
-    combinations.push([...current]);
+    });
   }
   return combinations;
 }
 
-// console.log(combinationA([]).length);
-// console.log(combinationA([1]).length);
-// console.log(combinationA([1, 2]).length);
-console.log(combinationA([1, 2, 3]));
-// console.log(combinationA([1, 2, 3, 4]));
+console.log(combinationA([1, 2]).length, FN(2));
+console.log(combinationA([1, 2, 3, 4]).length, FN(4));
+console.log(combinationA([1, 2, 3, 4, 5]).length, FN(5));
+
+function AN(n: number): number {
+  if (n === 0) return 0;
+  if (n === 1 || n === 2) return 2;
+  return n * AN(n - 1);
+}
+
+function FN(n: number): number {
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+  return AN(n) + n * FN(n - 1)
+}
