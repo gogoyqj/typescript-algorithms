bde3c685edf7d6466e2e979b85607c641ed13960
skipper <skipper>
2021-11-18 11:55:17 +0800

    refactor: finish ZAlgorithm

--- src/algorithms/Strings/ZAlgorithm.ts
+++ src/algorithms/Strings/ZAlgorithm.ts
@@ -1,6 +1,34 @@
 function ZAlgorithm(zString: string) {
-  const zArray: number[] = Array(zString.length);
+  const zArray: number[] = Array(zString.length).fill(-1);
+  let zBoxLeft = 0;
+  let zBoxRight = 0;
+  let zBoxShift = 0;
+  for (let i = 1; i < zString.length; i++) {
+    if (i > zBoxRight) {
+      zBoxLeft = zBoxRight = i;
+      // zBoxRight - i 更好理解
+      // zBoxRight - i === zBoxRight - zBoxLeft
+      while (zBoxRight < zString.length && zString[zBoxRight] === zString[zBoxRight - zBoxLeft]) {
+        zBoxRight++;
+      }
+      zArray[i] = zBoxRight - zBoxLeft;
+      zBoxRight = zBoxRight - 1;
+    } else {
+      zBoxShift = i - zBoxLeft;
+      if (zArray[zBoxShift] < (zBoxRight - i) + 1) {
+        zArray[i] = zArray[zBoxShift];
+      } else {
+        zBoxLeft = i;
+        while (zBoxRight < zString.length && zString[zBoxRight] === zString[zBoxRight - zBoxLeft]) {
+          zBoxRight++;
+        }
+        zArray[i] = zBoxRight - zBoxLeft;
+        zBoxRight = zBoxRight - 1;
+      }
+    }
+  }
   return zArray;
 }
 
-console.log(ZAlgorithm('123fgh123'))
+console.log(ZAlgorithm('123fg12fh123fg'));
+console.log(ZAlgorithm('1111111'))
