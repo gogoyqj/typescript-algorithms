f9fa8219ac1e8bef44a06aacd942f3a9516718da
skipper <skipper>
2021-11-22 17:39:18 +0800

    refactor: add LongestIncreasingSubsequenceV2

--- src/algorithms/Sets/LongestIncreasingSubsequence.ts
+++ src/algorithms/Sets/LongestIncreasingSubsequence.ts
@@ -14,3 +14,21 @@ export function LongestIncreasingSubsequence(sequence: number[]) {
 console.log(LongestIncreasingSubsequence([1, 2, 3, 4, 1, 5, 3, 6]));
 console.log(LongestIncreasingSubsequence([0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]));
 
+export function LongestIncreasingSubsequenceV2(sequence: number[]) {
+  const sequenceArr: number[][] =sequence.length > 0 ? [[sequence[0]]] : [];
+  for (let index = 1; index < sequence.length; index++) {
+    for (let i = sequenceArr.length - 1; i >= 0; i--) {
+      const row = sequenceArr[i] = sequenceArr[i] || [];
+      if (row.find((ele) => (ele <= sequence[index])) !== undefined) {
+        const nextRow = sequenceArr[i + 1] = sequenceArr[i + 1] || [];
+        nextRow.push(sequence[index]);
+        break;
+      } else if (i < 1) {
+        sequenceArr[0].push(sequence[index]);
+      }
+    }
+  }
+  return sequenceArr.length;
+}
+console.log(LongestIncreasingSubsequenceV2([1, 2, 3, 4, 1, 5, 3, 6]));
+console.log(LongestIncreasingSubsequenceV2([0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]));
